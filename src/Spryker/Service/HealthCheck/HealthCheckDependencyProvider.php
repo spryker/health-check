00a54c337511184b53e10b54ab1bb78f56f34501
Oleksandr Myrnyi <oleksandr.myrnyi@spryker.com>
2019-11-11 17:00:07 +0200

    SC-1267: Basic module structure

--- src/Spryker/Service/HealthCheck/HealthCheckDependencyProvider.php
+++ src/Spryker/Service/HealthCheck/HealthCheckDependencyProvider.php
@@ -8,7 +8,43 @@
 namespace Spryker\Service\HealthCheck;
 
 use Spryker\Service\Kernel\AbstractBundleDependencyProvider;
+use Spryker\Service\Kernel\Container;
 
 class HealthCheckDependencyProvider extends AbstractBundleDependencyProvider
 {
+    public const PLUGINS_HEALTH_CHECK = 'PLUGINS_HEALTH_CHECK';
+
+    /**
+     * @param \Spryker\Service\Kernel\Container $container
+     *
+     * @return \Spryker\Service\Kernel\Container
+     */
+    public function provideServiceDependencies(Container $container)
+    {
+        $container = $this->addHealthCheckPlugins($container);
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Service\Kernel\Container $container
+     *
+     * @return \Spryker\Service\Kernel\Container
+     */
+    protected function addHealthCheckPlugins(Container $container): Container
+    {
+        $container->set(static::PLUGINS_HEALTH_CHECK, function () {
+            return $this->getHealthCheckPlugins();
+        });
+
+        return $container;
+    }
+
+    /**
+     * @return \Spryker\Service\HealthCheckExtension\Dependency\Plugin\HealthCheckPluginInterface[]
+     */
+    protected function getHealthCheckPlugins(): array
+    {
+        return [];
+    }
 }
