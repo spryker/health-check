85d065324e9d7f7cddf723972cf2d558fa72bf9a
Oleksandr Myrnyi <oleksandr.myrnyi@spryker.com>
2019-11-12 12:29:59 +0200

    SC-1264: Basic HealthChecks

---
+++ src/Spryker/Client/HealthCheck/Zed/HealthCheckZedStubInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Client\HealthCheck\Zed;
+
+use Generated\Shared\Transfer\HealthCheckServiceResponseTransfer;
+
+interface HealthCheckZedStubInterface
+{
+    /**
+     * @return \Generated\Shared\Transfer\HealthCheckServiceResponseTransfer
+     */
+    public function doHealthCheck(): HealthCheckServiceResponseTransfer;
+}
